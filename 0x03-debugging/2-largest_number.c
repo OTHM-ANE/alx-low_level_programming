4aa15cf0c66464e02d871385b4ded381faf6edaf
OTHM-ANE <bnaninofal@gmail.com>
2023-03-30 09:00:57 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,16 @@ int largest_number(int a, int b, int c)
 
 	if (a >= b && a >= c)
 	{
-		largest = a;
+	largest = a;
 	}
 	else if (b >= a && b >= c)
 	{
-		largest = b;
+	largest = b;
 	}
-	else if (c >= a && c >= b)
+	else
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
 
